77cba1ab9dc37ce9073ea4964d616f202a099767
OZoneGuy <oalkersh@protonmail.com>
2023-08-15 21:28:17 -0400

    feat - Using auth middleware
    And added some body to the request

--- src/s3.rs
+++ src/s3.rs
@@ -1,21 +1,28 @@
-use actix_identity::Identity;
 // use actix_identity::IdentityExt as _;
 use actix_web::{
     get,
     web::{scope, Query, ServiceConfig},
     HttpResponse,
 };
+use actix_web_lab::middleware::from_fn;
+
+use crate::auth::auth_middleware;
 
 pub fn s3_config(cfg: &mut ServiceConfig) {
-    cfg.service(scope("/s3").service(list_objects).service(get_object));
+    cfg.service(
+        scope("/s3")
+            .wrap(from_fn(auth_middleware))
+            .service(list_objects)
+            .service(get_object),
+    );
 }
 
 #[get("/list_objects")]
-async fn list_objects(path: Option<Query<String>>, _id: Identity) -> HttpResponse {
-    HttpResponse::Ok().body("list_objects")
+async fn list_objects(path: Option<Query<String>>) -> HttpResponse {
+    HttpResponse::Ok().body(format!("list_objects: {:?}", path))
 }
 
 #[get("/get_object")]
-async fn get_object(file_path: Query<String>, _id: Identity) -> HttpResponse {
-    HttpResponse::Ok().body("get_object")
+async fn get_object(file_path: Query<String>) -> HttpResponse {
+    HttpResponse::Ok().body(format!("get_object: {:?}", file_path))
 }
